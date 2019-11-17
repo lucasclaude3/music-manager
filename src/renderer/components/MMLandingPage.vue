bb62015add9548b509404d8b30229d74040398e9
kahlua <lucasclaude3@gmail.com>
2019-11-17 18:02:39 +0100

    Create basic Autocomplete component

---
+++ src/renderer/components/MMLandingPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="wrapper">
+    <main>
+      <div class="title">Music Manager</div>
+      <new-tag></new-tag>
+    </main>
+  </div>
+</template>
+
+<script>
+  import NewTag from './MMLandingPage/NewTag';
+
+  export default {
+    name: 'mm-landing-page',
+    components: { NewTag },
+    methods: {
+      open(link) {
+        this.$electron.shell.openExternal(link);
+      },
+    },
+  };
+</script>
+
+<style>
+</style>
---
+++ src/renderer/components/MMLandingPage/NewTag.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="title">New Tag</div>
+    <div class="container-item">
+      <autocomplete label="Standard items:" placeholder="span, div, h1..." />
+    </div>
+  </div>
+</template>
+
+<script>
+  import Autocomplete from './Autocomplete';
+
+  export default {
+    name: 'new-tag',
+    components: { Autocomplete },
+  };
+</script>
+
+<style scoped>
+</style>

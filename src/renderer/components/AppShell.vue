4f61b743c65f543c3d139a8e4636734c17220b36
Lucas CLAUDE <lucasclaude3@gmail.com>
2019-12-24 17:30:34 +0100

    Create app shell (#11)
    * Create App Shell
    
    * Create vars scss file and load it into a component

---
+++ src/renderer/components/AppShell.vue
@@ -0,0 +1,32 @@
+<template>
+  <main>
+    <Reader />
+    <div class="wrapper">
+      <Sidebar />
+      <MainWindow />
+    </div>
+  </main>
+</template>
+
+<script>
+import Reader from './Reader';
+import Sidebar from './Sidebar';
+import MainWindow from './MainWindow';
+
+export default {
+  name: 'AppShell',
+  components: {
+    Reader,
+    Sidebar,
+    MainWindow,
+  },
+};
+</script>
+
+<style lang="scss">
+  .wrapper {
+    display: flex;
+    width: 100%;
+    align-items: stretch;
+  }
+</style>
---
+++ src/renderer/components/Sidebar.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav class="sidebar">
+    sidebar
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Sidebar',
+};
+</script>
+
+<style scoped lang="scss">
+  @import 'styles/_vars.scss';
+
+  .sidebar {
+    min-width: 200px;
+    max-width: 200px;
+    min-height: 100vh;
+    border-right: 1px solid $border-primary;
+    background-color: $background-primary;
+    color: $text-primary;
+  }
+</style>
